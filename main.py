2e6c150e6aa83c247e0e0ca928c3db4c844312b3
FeepingCreature <default_357-line@yahoo.de>
2019-10-29 23:48:43 +0100

    add sidebar treeview, enable trace support

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import os
 from os import path
 
@@ -40,6 +41,11 @@ class AlternatingPair:
             else:
                 self.alphas.append('a')
 
+    def list(self):
+        if self.digits:
+            return [''.join(self.alphas), ''.join(self.digits)]
+        return [''.join(self.alphas)]
+
     def depth(self):
         if self.digits:
             return 2
@@ -76,6 +82,9 @@ class AlternatingPairs:
 
     def increment(self):
         self.pairs[-1].increment()
+
+    def list(self):
+        return [item for pair in self.pairs for item in pair.list()]
 
     def depth(self):
         return sum(pair.depth() for pair in self.pairs)
@@ -119,7 +128,7 @@ class Zettel:
         return path.exists(self.file)
 
 class Trace:
-    def __init__(self, file):
+    def __init__(self, file = u'data/trace.txt'):
         self.file = file
         self.names = []
         self.read()
@@ -131,9 +140,20 @@ class Trace:
         except FileNotFoundError:
             pass
 
+    def insert(self, after, name):
+        index = self.names.index(after)
+        self.names.insert(index + 1, name)
+
+    def append(self, new_name):
+        self.remove(new_name)
+        self.names.append(new_name)
+
+    def remove(self, remove_name):
+        self.names = [name for name in self.names if not name == remove_name]
+
     def write(self):
         with open(self.file, 'w') as file:
-            file.write(self.names.join('\n'))
+            file.write('\n'.join(self.names))
 
 class ToggleHack:
     @staticmethod
@@ -150,6 +170,96 @@ def load_zettels():
                 for file in files]
     return {zettel.name: zettel for zettel in [Zettel(file=file) for file in filelist]}
 
+class GraphicalTreeNode:
+    def __init__(self, zettel, states):
+        self.zettel = zettel
+        self.states = states
+        self.name = self.zettel.name
+
+    def indentmarker(self):
+        def marker(state):
+            if state == TreeState.SINGLE:
+                return '─'
+            if state == TreeState.FIRST:
+                return '┬'
+            if state == TreeState.LAST:
+                return '└'
+            if state == TreeState.NONE:
+                return '&nbsp;'
+            if state == TreeState.PASS:
+                return '│'
+            if state == TreeState.FORK:
+                return '├'
+            assert False
+
+        return ''.join([marker(state) for state in self.states]) + ' '
+
+class TreeState(Enum):
+    SINGLE = 1
+    FIRST = 2
+    PASS = 3
+    FORK = 4
+    LAST = 5
+    NONE = 6
+
+class TreeNode:
+    def __init__(self):
+        self.zettel = None
+        self.children = {}
+
+    def list(self):
+        children = [self.children[name] for name in sorted(self.children.keys())]
+
+        childlist = []
+        if self.zettel:
+            childlist.insert(0, GraphicalTreeNode(self.zettel, []))
+
+        for i, child in enumerate(children):
+            childlist += child.list()
+
+        num_branches = sum(1 for child in childlist if child.states and (
+            child.states[0] == TreeState.FIRST or child.states[0] == TreeState.SINGLE))
+        branches = 0
+        for i, child in enumerate(childlist):
+            if num_branches and branches == num_branches:
+                child.states.insert(0, TreeState.NONE)
+            elif num_branches and child.states and (
+                child.states[0] == TreeState.FIRST or child.states[0] == TreeState.SINGLE):
+                branches += 1
+                child.states.insert(0, TreeState.LAST if branches == num_branches else TreeState.FORK)
+            elif len(childlist) == 1:
+                child.states.insert(0, TreeState.SINGLE)
+            elif i == 0:
+                child.states.insert(0, TreeState.FIRST)
+            elif i == len(childlist) - 1:
+                child.states.insert(0, TreeState.LAST)
+            else:
+                child.states.insert(0, TreeState.PASS)
+
+        return childlist
+
+def tree_insert(tree, keys, zettel):
+    if not tree:
+        tree = TreeNode()
+
+    try:
+        key = next(keys)
+    except StopIteration:
+        tree.zettel = zettel
+        return tree
+
+    tree.children[key] = tree_insert(tree.children.get(key), keys, zettel)
+    return tree
+
+def prefix_sort(zettels):
+    tree = None
+
+    for name, zettel in zettels.items():
+        keys = AlternatingPairs(name).list()
+
+        tree = tree_insert(tree, iter(keys), zettel)
+    return tree
+
 @app.route('/zettel/<name>/edit', methods=['POST'])
 def edit(name):
     global root_path
@@ -157,15 +267,45 @@ def edit(name):
     zettel.write(content=request.form['content'])
     return redirect(root_path)
 
+@app.route('/zettel/<name>/insert/<next_name>', methods=['POST'])
+def insert(name, next_name):
+    global root_path
+    zettel = Zettel(name=next_name)
+
+    trace = Trace()
+    trace.insert(after=name, name=next_name)
+    trace.write()
+
+    zettel.write(content=request.form['content'])
+    return redirect(root_path)
+
+@app.route('/trace/<name>/add', methods=['POST'])
+def add_trace(name):
+    global root_path
+    trace = Trace()
+    trace.append(name)
+    trace.write()
+    return redirect(root_path)
+
+@app.route('/trace/<name>/remove', methods=['POST'])
+def remove_trace(name):
+    global root_path
+    trace = Trace()
+    trace.remove(name)
+    trace.write()
+    return redirect(root_path)
+
 @app.route('/')
 def index():
     zettels = load_zettels()
-    trace = Trace("data/trace.txt")
-    # names = trace.names
-    names = sorted(zettels.keys())
+    trace = Trace()
+    names = sorted(trace.names)
+
+    zetteltree = prefix_sort(zettels).list()
     context = {
         'names': names,
-        'zettels': [zettels[name] for name in names],
+        'zetteltree': zetteltree,
+        'trace': [zettels[name] for name in names],
         'toggle': ToggleHack,
     }
     return render_template('index.html', **context)
